9b8a3fe1f524fe700ae18edc4f21e51afb0b6f2e
tfluhr <35077186+tfluhr@users.noreply.github.com>
2022-03-09 10:53:46 -0600

    Update open_alex_api_call.py

--- open_alex_api_call.py
+++ open_alex_api_call.py
@@ -1,12 +1,11 @@
 import urllib, requests, json, math
 
 #DECLARE SOME STUFF
-# counter to track api page 
-i = 1 # counter
-#API Call
-call = ('https://api.openalex.org/works?filter=institutions.id:I180949307&page=%s&sort=publication_date:desc&per-page=200' % i)
-#DIR TO DUMP JSON FILES
-json_dir = r'C:\Users\tfluhr\Desktop\json_dir'
+i = 1 # SIMPLE COUNTER
+header = "mailto=tfluhr@iit.edu" # ADD HEADER TO API CALL IN ORDER TO GET IN POLITE POOL
+uid = "I180949307" # OPENALEX INSTITUTION ID
+call = ('https://api.openalex.org/works?filter=institutions.id:{}&page=1&sort=publication_date:desc&per-page=200&{}'.format(uid, header))
+json_dir = r'C:\Users\tfluhr\Desktop\json_dir' # WHERE TO DUMP JSON 
 
 # GET OBJECT COUNT
 
@@ -24,7 +23,7 @@ print(api_page_count)
 #  Get all the JSON
 
 while i <= api_page_count:
-    call = ('https://api.openalex.org/works?filter=institutions.id:I180949307&page=%s&sort=publication_date:desc&per-page=200' % i)
+    call = ('https://api.openalex.org/works?filter=institutions.id:{}&page={}&sort=publication_date:desc&per-page=200&{}'.format(uid, i, header))
     response = requests.get(call)
     with open(json_dir + "\\" + str(i) + ".json" , 'wb') as file:
         file.write(response.content)
